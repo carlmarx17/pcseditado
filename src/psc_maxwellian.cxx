e19e7bf285db0ceb62c827fc72ee1fe8a09683a8
carlmarx17 <cmartinezsi@unal.edu.co>
2026-04-16 23:11:12 -0500

    Revert "Change domain decomposition to 56 tasks"
    This reverts commit 49469c853e848c14a6df9e5e9b360d3d79dd3c0c.

--- src/psc_maxwellian.cxx
+++ src/psc_maxwellian.cxx
@@ -187,7 +187,7 @@ Grid_t* setupGrid()
 
   Grid_t::Real3 LL{1., kResolvedDomainDe, kResolvedDomainDe};
   Int3 gd{1, finalGridYZ, finalGridYZ};
-  Int3 np{1, 7, 8}; // 1 * 7 * 8 = 56 MPI tasks
+  Int3 np{1, 4, 8};
 
   Grid_t::Domain dom{gd, LL, -.5 * LL, np};
   psc::grid::BC bc{{BND_FLD_PERIODIC, BND_FLD_PERIODIC, BND_FLD_PERIODIC},
