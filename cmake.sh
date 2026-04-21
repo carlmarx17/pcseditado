8fde160c0f3684d2a6fac848bac090b801aa70f9
carlmarx17 <cmartinezsi@unal.edu.co>
2026-04-21 07:39:38 -0500

    Update cmake.sh with ADIOS2 prefix path placeholder

--- cmake.sh
+++ cmake.sh
@@ -2,6 +2,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
     -DPSC_USE_ADIOS2=ON \
+    -DCMAKE_PREFIX_PATH="/path/to/adios2/install" \
     -DUSE_CUDA=OFF \
     -DUSE_VPIC=OFF \
     ..
